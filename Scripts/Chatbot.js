b228d89c401831ec019c4a140423078882bd8a54
Javier-H3 <hernandezgomezjavier369@gmail.com>
2024-06-15 00:03:02 -0500

    Correcciones al chatBot

--- Scripts/Chatbot.js
+++ Scripts/Chatbot.js
@@ -41,7 +41,7 @@ function sendMessage(option = null) {
 
     const configBot = document.createElement('div');
     configBot.className = 'message bot-message';
-    configBot.innerHTML = `<div>${getBotResponse(mensaje)}</div>`;
+    configBot.innerHTML = `<p>${getBotResponse(mensaje)}</p>`;
 
     contenedorMensajeBot.appendChild(contenedorLogo);
     contenedorMensajeBot.appendChild(configBot);
@@ -57,9 +57,9 @@ function sendMessage(option = null) {
 
 function getBotResponse(mensaje) {
     const responses = {
-        '¿Qué hago?': 'SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS.',
-        '¿Cómo puedo ayudarte?': 'Estoy aquí para asistirte con cualquier pregunta o problema que tengas.',
-        '¡Hablemos!': '¡Claro! ¿Sobre qué te gustaría hablar?'
+        '¿Qué hago?': 'Mi misión es ayudarte a identificar y definir el código CIIU correcto para tu empresa o actividad económica. Ya sea que estés iniciando un nuevo negocio, necesites actualizar tu información, o simplemente tengas curiosidad sobre qué código corresponde a tus actividades,  ¡yo estoy aquí para guiarte!',
+        '¿Cómo puedo ayudarte?': '1. Búsqueda de Códigos: Te ayudo a encontrar el código CIIU que mejor se ajuste a lo que hace tu empresa. 2. Registro y Actualización: Te doy orientación sobre cómo y dónde registrar o actualizar tu código CIIU. 3. Información Clara: Respondo todas tus preguntas sobre los códigos CIIU de manera sencilla y fácil de entender. 4. Asesoramiento Personalizado: Si tienes actividades económicas variadas, te ayudo a clasificar correctamente tus actividades principales y secundarias.',
+        '¡Hablemos!': '¿Tienes una pregunta sobre tu código CIIU? ¿No estás seguro de qué código elegir? ¡No te preocupes! Haz clic en el botón de chat y comencemos a trabajar juntos para resolver todas tus dudas.'
     };
 
     return responses[mensaje] || 'Lo siento, no entendí eso. ¿Puedes reformular tu pregunta?';
